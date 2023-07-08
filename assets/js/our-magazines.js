ceac3ffb4b1e4211ab2a34b7e1ff2cd4a7053f57
snehasudhakaran <sudhakaransneha30@gmail.com>
2023-07-08 10:38:29 +0530

    updated images,logo and content for their respective pages

--- assets/js/our-magazines.js
+++ assets/js/our-magazines.js
@@ -1,30 +1,17 @@
-let pagination1 = document.querySelector(".group-1");
-let pagination2 = document.querySelector(".group-2");
-let pagination3 = document.querySelector(".group-3");
+let prev_content = document.querySelector(".group-1");
+let next_content = document.querySelector(".group-2");
+
 
 function first(){
-    pagination1.style.display ="block";
-    pagination2.style.display = "none";
-    pagination3.style.display = "none";
-    document.querySelector(".one").style.backgroundColor="#ffb23b";
-    document.querySelector(".two").style.backgroundColor="transparent";
-    document.querySelector(".three").style.backgroundColor="transparent";
-}
+    prev_content.style.display ="block";
+    next_content.style.display = "none";
+    document.querySelector(".prev-button").style.backgroundColor="#ffb23b";
+    document.querySelector(".next-button").style.backgroundColor="grey";
+};
 
 function second(){
-    pagination1.style.display ="none";
-    pagination2.style.display = "block";
-    pagination3.style.display = "none";
-    document.querySelector(".one").style.backgroundColor="transparent";
-    document.querySelector(".two").style.backgroundColor="#ffb23b";
-    document.querySelector(".three").style.backgroundColor="transparent";
-}
-
-function third(){
-    pagination1.style.display ="none";
-    pagination2.style.display = "none";
-    pagination3.style.display = "block";
-    document.querySelector(".one").style.backgroundColor="transparent";
-    document.querySelector(".two").style.backgroundColor="transparent";
-    document.querySelector(".three").style.backgroundColor="#ffb23b";
-}
+    prev_content.style.display ="none";
+    next_content.style.display = "block";
+    document.querySelector("#prev-button").style.backgroundColor="grey";
+    document.querySelector(".next-button").style.backgroundColor="#ffb23b";
+};
